3bd9590044ef912ac6911c6ab7655519cc1d72cb
jovanlekic12 <lekicjovan1212@gmail.com>
2024-09-20 18:30:00 +0200

    add methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
+import Menu from "./menu";
+import data from "./data";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [meals, setMeal] = useState(data);
 
-  return <></>;
+  return (
+    <div className="Container">
+      <h1 className="title">Our Menu</h1>
+      <ul className="list">
+        {meals.map((meal) => {
+          return <Menu key={meal.id} {...meal}></Menu>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
